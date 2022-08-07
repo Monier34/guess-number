d422d672f0ea895a0f8d0cb3a2374c95c83e549b
monier34 <santerjov@gmail.com>
2022-08-07 13:47:15 +0300

    little update

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ UserGuess = -1
 while UserGuess != NumberToGuess:
     UserGuess = int(input("Угадай число от 1 до 100"))
     if UserGuess > NumberToGuess:
-        print("Число должно быть меньше!")
+        print("Число должно быть меньше! ")
     elif UserGuess < NumberToGuess:
-        print("Число должно быть  больше!")
+        print("Число должно быть  больше! ")
     else:
-        print("Вы угадали, этл число" + str(NumberToGuess))
-    break
+        print("Вы угадали, этл число " + str(NumberToGuess))
+        break
